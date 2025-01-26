e5c92e32fd2cce252028434f01aba16629265388
t-usman2010 <t.usman36412@gmail.com>
2025-01-26 13:19:35 +0500

    Project

--- Class 3/scrpit.js
+++ Class 3/scrpit.js
@@ -1,12 +1,14 @@
 var start_num = parseInt(prompt("Enter Starting Number: "));
 var end_num = parseInt(prompt("Enter Ending Number: "));
+let nan = 0;
 
 for(var i= start_num; i <=end_num; i++){
     var resutl = start_num*i;
     if(resutl%2==0){
-        document.write("Even Number -->" + i + "==" + resutl+ "</br>");
+        nan = resutl;
+        document.write("Even Number -->"+ start_num + "*" + i + "==" + resutl+"is higher than"+ ((i-1)*start_num) + "</br>");
     }
     else{
-        document.write("Odd Number -->" + i + "==" + resutl+ "</br>");
+        document.write("Odd Number -->" + start_num + "*"+ i + "==" + resutl + "is higher than"+ i/start_num+ "</br>");
     }
 }
