1f8c06e4f0e1f612f2155d4e1714529eef3b2943
t-usman2010 <t.usman36412@gmail.com>
2025-02-02 14:03:14 +0500

    Project

---
+++ Class 4/Project 2/script.js
@@ -0,0 +1,9 @@
+function mm(){
+    let num = [1,2,3,,4,5,6,7]
+
+for (let i=[num.length];i>=[i+1]; i>m) {
+
+    m = [i+2]
+    console.log(m)
+}
+}
--- Class 4/script.js
+++ Class 4/script.js
@@ -1,33 +1,16 @@
-const form = document.getElementById('myForm');
 
-        form.addEventListener('submit', function(event) {
-            event.preventDefault();
+function num1(){
 
-            const name = document.getElementById('name').value;
-            const email = document.getElementById('email').value;
-            const phone = document.getElementById('phone').value;
-            const address = document.getElementById('address').value;
-            const gender = document.getElementById('gender').value;
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let phone = document.getElementById("phone").value;
+    let address = document.getElementById("address").value;
+    let gender = document.getElementById("gender").value
 
-            const emailParams = {
-                user_name: name,       // Matches {{user_name}} in the template
-                user_email: email,     // Matches {{user_email}} in the template
-                user_phone: phone,     // Matches {{user_phone}} in the template
-                user_address: address, // Matches {{user_address}} in the template
-                user_gender: gender, // Matches {{user_gender}} in the template
-            };
-
-            // Replace with your Service ID and Template ID
-            const serviceId = 'service_o0ahbqk';  // Your service id
-            const templateId = 'template_0r30o63'; // Your template id
-
-            emailjs.send(serviceId, templateId, emailParams)
-                .then(function(response) {
-                    console.log('SUCCESS!', response.status, response.text);
-                    alert('Message sent successfully!');
-                    form.reset(); // Clear the form after successful submission
-                }, function(error) {
-                    console.error('FAILED...', error);
-                    alert('An error occurred. Please try again later.');
-                });
-        });
+    h1.innerText = name;
+    h2.innerText = email;
+    h3.innerText = phone;
+    h4.innerText = address;
+    h5.innerText = gender;
+    console.log(name);
+}
