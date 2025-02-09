2509b2309566ef3e28c1faf23182c7311d6122fa
t-usman2010 <t.usman36412@gmail.com>
2025-02-09 11:57:00 +0500

    Project

---
+++ Class 5/script.js
@@ -0,0 +1,14 @@
+function myFunction() {
+    let x = [15,18,20,27,];
+    for(let i=0; i<x.length; i++){
+        if(x[i] % 3 === 0 && x[i] % 5 === 0){
+                document.write("FizzBizz"+ x[i] + "<br>");
+        }
+        else if (x[i] % 3 === 0){
+            document.write("Fizz"+ x[i] + "<br>");
+        }
+        else if (x[i] % 5 === 0){
+            document.write("Bizz"+ x[i] + "<br>");
+        }
+    }
+} 
