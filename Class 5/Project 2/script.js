d009fdaa083dd5ee7cc53c913ce60e5b8183b22e
t-usman2010 <t.usman36412@gmail.com>
2025-02-10 21:46:06 +0500

    Project

--- Class 5/Project 2/script.js
+++ Class 5/Project 2/script.js
@@ -1,16 +1,8 @@
+let y = document.getElementById("demo");
+let colour = ["red"];
 function myFunction() {
-    let x = [15,18,20,27,];
-    x.push =(30)
-    x.pop =(27)
-    for(let i=0; i<x.length; i++){
-        if(x[i] % 3 === 0 && x[i] % 5 === 0){
-                document.write("FizzBizz"+ x[i] + "<br>");
-        }
-        else if (x[i] % 3 === 0){
-            document.write("Fizz"+ x[i] + "<br>");
-        }
-        else if (x[i] % 5 === 0){
-            document.write("Bizz"+ x[i] + "<br>");
-        }
-    }
-} 
+    let user_input = document.getElementById("colur").value;
+    colour.push(user_input);
+    y.innerHTML = colour	
+
+}
