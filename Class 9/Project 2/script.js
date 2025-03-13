b2675f560cc4dfa89f98957588f4b71d7af017fc
t-usman2010 <t.usman36412@gmail.com>
2025-03-13 11:23:38 +0500

    Project

--- Class 9/Project 2/script.js
+++ Class 9/Project 2/script.js
@@ -1,6 +1,45 @@
+//Showing Time 
 function showTime() {
 	document.getElementById('currentTime').innerHTML = new Date().toLocaleString();
 }
+
+// Date
+function date(){
+    let iftar_time = ["6:12","6:13","6:13","6:14","6:15","6:16","6:17",]
+    let date_module = new Date();
+    let date = date_module.getDay();
+    document.getElementById('date').innerHTML = ("Todays Iftar Time is " + iftar_time[date]);
+}
+
+
+function Backgrond(){
+    AOS.init({
+        duration: 1000,
+        once: false,
+        mirror: true
+    });
+    
+    
+// Add random floating particles in the background
+    const body = document.querySelector('body');
+    for(let i = 0; i < 50; i++) {
+        const particle = document.createElement('div');
+        particle.style.position = 'fixed';
+        particle.style.width = '2px';
+        particle.style.height = '2px';
+        particle.style.background = 'rgba(255,255,255,0.5)';
+        particle.style.left = Math.random() * 100 + 'vw';
+        particle.style.top = Math.random() * 100 + 'vh';
+        particle.style.animation = `float ${Math.random() * 3 + 2}s infinite`;
+        body.appendChild(particle);
+    }
+    
+}
+
+
+//Calling Funtions
+date();
+Backgrond();
 showTime();
 setInterval(function () {
 	showTime();
